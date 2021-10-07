8d07fddf8dae7b1754b6a04d9ca217273008a564
MacbookAir <fmdentiste@pm.me>
2021-10-07 14:11:24 +0200

    ajout header page error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom'
 import '../styles/Error.css'
 import { Component } from 'react'
+import Header from './Header'
 
 class Error extends Component {
     render() {
         return (
-            <main className="error__404">
-                <h2>404</h2>
-                <h3>Oups ! La page que vous demandez n'existe pas.</h3>
-                <Link to="/">Retournez sur la page d'accueil</Link>
-            </main>
+            <>
+                <Header />
+                <main className="error__404">
+                    <h2>404</h2>
+                    <h3>Oups ! La page que vous demandez n'existe pas.</h3>
+                    <Link to="/">Retournez sur la page d'accueil</Link>
+                </main>
+            </>
         )
     }
 }
